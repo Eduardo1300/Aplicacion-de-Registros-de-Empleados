0cad6f14a1938f22c1bb732a0d81dcf9bf5561df
Eduardo Valdivia <eduardovaldivia130@outlook.es>
2026-02-21 18:59:06 -0500

    chore: remove old Vue frontend and unnecessary files

--- fix_usuarios.sql
+++
@@ -1,3 +0,0 @@
-UPDATE "usuarios" SET "nombreUsuario" = 'admin' WHERE id = 1;
-UPDATE "usuarios" SET "nombreUsuario" = 'empleado' WHERE id = 2;
-SELECT id, nombreusuario, "nombreUsuario" FROM "usuarios";
